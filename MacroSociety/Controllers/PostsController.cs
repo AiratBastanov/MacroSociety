56c23113f6238f9025897bebc552109111a2c098
Айрат Бастанов <BastanovAI@stud.kai.ru>
2023-10-09 19:39:13 +0300

    промежуточные изменения - добавлен метод по возврату постов и лайков одновременно.

--- MacroSociety/Controllers/PostsController.cs
+++ MacroSociety/Controllers/PostsController.cs
@@ -6,6 +6,8 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using MacroSociety.Models;
+using System.Text.Json.Serialization;
+using System.Text.Json;
 
 namespace WebApiSociety.Controllers
 {
@@ -26,16 +28,7 @@ namespace WebApiSociety.Controllers
             IEnumerable<Post> myAL;
             myAL = await _context.Posts.Where(post => post.NameUser == name).ToListAsync();
             return myAL;
-        }    
-
-        [HttpPost]
-        public async Task<int> CreateNewPost(Post post)
-        {
-            _context.Posts.Add(post);
-            int result = await _context.SaveChangesAsync();
-            return result;
         }
-
 
         [HttpGet("myposts")]
         public async Task<IEnumerable<Post>> GetMyPost(string name)
@@ -43,6 +36,61 @@ namespace WebApiSociety.Controllers
             IEnumerable<Post> myAL;
             myAL = await _context.Posts.Where(post => post.NameUser == name).ToListAsync();
             return myAL;
+        }
+
+        [HttpGet("posts-and-likes")]
+        public async Task<IActionResult> GetPostsAndLikesWithPagination(int userId, int? friendId,string name, int page, int pageSize)
+        {
+            IQueryable<Post> query;
+
+            if (friendId.HasValue)
+            {
+                query = _context.Posts.Where(post => post.IdUser == friendId);              
+            }
+            else
+            {
+                query = _context.Posts.Where(post => post.IdUser == userId);
+            }
+
+            query = query.OrderByDescending(post => post.Id);
+
+            var totalPosts = await query.CountAsync();
+            var posts = await query
+                .Skip((page - 1) * pageSize)
+                .Take(pageSize)
+                .ToListAsync();
+
+            var postIds = posts.Select(post => post.Id).ToList();
+
+            var likes = await _context.Likes
+                .Where(like => postIds.Contains(like.IdFriendPost) && like.NameUserLike == name)
+                .ToListAsync();
+
+            var postsWithLikes = new PostWithLikes
+            {
+                Posts = posts,
+                Likes = likes
+            };
+
+            //var totalPages = (int)Math.Ceiling((double)totalPosts / pageSize);
+
+            var jsonSerializerOptions = new JsonSerializerOptions
+            {
+                ReferenceHandler = ReferenceHandler.Preserve
+            };
+
+            var json = JsonSerializer.Serialize(postsWithLikes, jsonSerializerOptions);
+
+            return Ok(json);
+        }
+
+
+        [HttpPost]
+        public async Task<int> CreateNewPost(Post post)
+        {
+            _context.Posts.Add(post);
+            int result = await _context.SaveChangesAsync();
+            return result;
         }
 
         [HttpDelete("deletepost")]
---
+++ MacroSociety/Models/PostWithLikes.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace MacroSociety.Models
+{
+    public class PostWithLikes
+    {
+        public List<Post> Posts { get; set; }
+        public List<Like> Likes { get; set; }
+    }
+
+}
